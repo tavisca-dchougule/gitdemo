09d3813be24109f5392b7b89ecab82ffa0eb5f52
unknown <dchougule@TRAINING7.internal.tavisca.com>
2015-07-05 17:21:42 +0530

    Frist commit

---
+++ OperatorOverloading/OperatorOverloading/Man.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+    class Man
+    {
+        
+        private double weight;  // Breadth of a box
+        private double height;   // Height of a box
+
+        // Overload + operator to add two Box objects.
+        public static Man operator +(Man b, Man c)
+        {
+            Man man = new Man();
+
+            man.weight = b.weight + c.weight;
+            man.height = b.height + c.height;
+            return man;
+        }
+
+        public double getHeight()
+        {
+            return height;
+        }
+
+        public double getWeight()
+        {
+            return weight;
+        }
+
+        public void setHeight(double height)
+        {
+            this.height = height;
+        }
+        public void setWeight(double weight)
+        {
+            this.weight = weight;
+        }
+
+    }
+
+    
---
+++ OperatorOverloading/OperatorOverloading/Tester.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class Tester
+{
+    static void Main(string[] args)
+    {
+        Man Man1 = new Man();   
+        Man Man2 = new Man();   
+        Man Man3 = new Man();   
+       
+
+      
+        Man1.setHeight(6.0);
+        Man1.setWeight(66.0);
+        
+
+      
+        Man2.setHeight(5.11);
+        Man2.setWeight(75.0);
+        
+        Console.WriteLine("Height of Man1 : " + Man1.getHeight());
+        Console.WriteLine("Weight of Man1 : " + Man1.getWeight());
+        Console.WriteLine();
+        
+        Console.WriteLine("Height of Man2 : " + Man2.getHeight());
+        Console.WriteLine("Weight of Man2 : " + Man2.getWeight());
+        Console.WriteLine();
+        
+        Man3 = Man1 + Man2; //here addition of two man object takes place.
+      
+        Console.WriteLine("Height of Man3 : "+ Man3.getHeight());
+        Console.WriteLine("Weight of Man3 : " + Man3.getWeight());
+        
+        Console.ReadKey();
+    }
+}
